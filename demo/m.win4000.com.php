e6301db80d8a509a352e068feed3ade65aa01c39
zhibin02.li <zhibin02.li@vipshop.com>
2017-06-28 19:21:40 +0800

    configuration

---
+++ demo/m.win4000.com.php
@@ -0,0 +1,10 @@
+<?php
+ini_set("memory_limit", "1024M");
+require dirname(__FILE__).'/../core/init.php';
+
+/* Do NOT delete this comment */
+/* 不要删除这段注释 */
+
+$configs = [
+    'name' => 'm.win4000.com',
+];
