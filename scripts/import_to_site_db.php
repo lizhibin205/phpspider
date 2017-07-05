ff7b204132cf9fbfde15941ceafdbf4a22a0ea52
zhibin02.li <zhibin02.li@vipshop.com>
2017-07-05 10:15:21 +0800

    import_to_site_db.php

--- scripts/import_to_site_db.php
+++ scripts/import_to_site_db.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(0);
 require dirname(__DIR__) . "/config/inc_config.php";
 
 //init mysql
@@ -11,18 +12,27 @@ $length = 10;
 $lastOffset = 0;
 while (true) {
     $cursor = mysql_query("SELECT * FROM `mv_spider_mv` LIMIT {$lastOffset},{$length}");
+	$preOffset = $lastOffset;
     while ($row = mysql_fetch_assoc($cursor)) {
         //var_dump($row);
         //break 2;
         list($image_id, $image_page) = explode("_", $row['mv_id']);
-        preg_match("/^([^（]*)（/", $row["mv_title"], $matches);
-        $image_title = $matches[1];
-        var_dump($matches);
-        break 2;
+        preg_match("/^(.*)（\d+\/(\d+)）$/", $row["mv_title"], $matches);
+        $image_title = mysql_escape_string($matches[1]);
+		$image_page_total = $matches[2];
+        //var_dump($matches);
+        //break 2;
+		$image_type = mysql_escape_string($row['mv_type']);
+		$image_from_url = mysql_escape_string($row['mv_image']);
         //insert to site
-        $sql = "INSERT INtO `mv_site_mv`(`image_id`,`image_page`,`image_title`,`image_type`,`image_url`,`image_from_url`)
-        VALUES({$image_id},{$image_page},'{$image_title}','{$image_type}','','{$image_from_url}')";
-        mysql_query($sql);
+        $sql = "INSERT INtO `mv_site_mv`(`image_id`,`image_page`,`image_page_total`,`image_title`,`image_type`,`image_url`,`image_from_url`)
+        VALUES({$image_id},{$image_page},{$image_page_total},'{$image_title}','{$image_type}','','{$image_from_url}')";
+        if (mysql_query($sql) === false) {
+			echo $sql, PHP_EOL;
+		}
         $lastOffset += 1;
     }
+	if ($lastOffset - $preOffset < $length) {
+		break;
+	}
 }
